8c4ed06a6808480c0d0f03465955ae07853c3188
Daniel Weber <dantech88@yahoo.com>
2019-09-15 23:15:26 -0400

    added basic block creation

--- bril-ocaml/main.ml
+++ bril-ocaml/main.ml
@@ -5,7 +5,16 @@ let main json_file () =
   let json_tree = from_file json_file in
   let program = Parse.parse_program json_tree in
   let program = Or_error.bind program ~f:Transform.transform_program in
-  printf !"%{sexp: Bril.program Or_error.t}" program
+  let blocks =
+    Or_error.map program ~f:(fun program ->
+        let blocks =
+          Basic_block.split_blocks (List.nth_exn program.functions 0).instrs
+        in
+        let blocks = Basic_block.prune_empty_blocks blocks in
+        Basic_block.label_blocks blocks )
+  in
+  printf !"%{sexp: Bril.program Or_error.t}\n\n" program ;
+  printf !"%{sexp: Bril.function_instruction list list Or_error.t}\n" blocks
 
 let () =
   let open Command.Let_syntax in
