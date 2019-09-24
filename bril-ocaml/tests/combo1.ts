94cb1762c1ed73615101be633a079d157f0cbe7a
Daniel Weber <dantech88@yahoo.com>
2019-09-23 22:58:31 -0400

    combo terminates

--- bril-ocaml/tests/combo1.ts
+++ bril-ocaml/tests/combo1.ts
@@ -2,8 +2,8 @@ let a = 32;
 let b = 12;
 for (let c = 0; c < 10; c = c) {
     if (a > b) {
-        for (let i = b; i < a; b = b + 1) {
-            console.log(a-b*c);
+        for (let i = b; i < a; i = i + 1) {
+            console.log(a-b*c + i/4);
         }
         b = a;
     } else if (a == b) {
@@ -11,6 +11,7 @@ for (let c = 0; c < 10; c = c) {
             console.log(c / 2);
         }
         c = 5;
+        b = a + 1;
     } else {
         for (let b = 0; b < a; b = b + 1) {
             console.log(c*b);
